741680dfea14b6b6b43c04fe493bc90cab73ff83
Mel Florance <mel.florance@gmail.com>
2017-08-01 12:48:44 +0200

    Add new rendering options for the core store module

--- app/src/store/modules/core/core.js
+++ app/src/store/modules/core/core.js
@@ -8,32 +8,51 @@
 import CST from '../../../core/utils/CST'
 import {CORE} from '../../mutations-types'
 
-const Store =
-{
-    state: {
-        canvas: {},
-        engine: {},
-        currentState: CST.STATES.EDITOR.ALWAYS,
-        options: {
-            viewport: {
-                showOptions: false,
-                showTools: false
+const Store = {
+    state     : {
+        canvas       : {},
+        engine       : {},
+        currentState : CST.STATES.EDITOR.ALWAYS,
+        options      : {
+            viewport  : {
+                showOptions : false,
+                showTools   : false
+            },
+            rendering : {
+                frameRange : {
+                    start : 0,
+                    end   : 600
+                },
+                resolution : new BABYLON.Vector2(1920, 1080)
             }
         }
     },
-    getters: {
-        isViewportOptionsVisible: state => state.options.viewport.showOptions,
-        isViewportToolsVisible: state => state.options.viewport.showTools,
-        getEditorState: state => state.currentState,
-        getCanvas: state => state.canvas,
-        getEngine: state => state.engine
+    getters   : {
+        isViewportOptionsVisible : state => state.options.viewport.showOptions,
+        isViewportToolsVisible   : state => state.options.viewport.showTools,
+        getEditorState           : state => state.currentState,
+        getCoreOptions           : state => state.options,
+        getCanvas                : state => state.canvas,
+        getEngine                : state => state.engine
     },
-    actions: {
+    actions   : {
         initCore(store, payload) {
             store.commit(CORE.INITIALIZE, payload);
         },
         setCurrentState(store, payload) {
             store.commit(CORE.SET_STATE, payload);
+        },
+        setRenderingResolutionX(store, payload) {
+            store.commit(CORE.SET_RENDERING_RESOLUTION_X, payload);
+        },
+        setRenderingResolutionY(store, payload) {
+            store.commit(CORE.SET_RENDERING_RESOLUTION_Y, payload);
+        },
+        setRenderingFrameRangeStart(store, payload) {
+            store.commit(CORE.SET_RENDERING_FRAME_RANGE_START, payload);
+        },
+        setRenderingFrameRangeEnd(store, payload) {
+            store.commit(CORE.SET_RENDERING_FRAME_RANGE_END, payload);
         },
         toggleViewportOptions(store, payload) {
             store.commit(CORE.TOGGLE_VIEWPORT_OPTIONS, payload)
@@ -42,9 +61,21 @@ const Store =
             store.commit(CORE.TOGGLE_VIEWPORT_TOOLS, payload)
         }
     },
-    mutations: {
+    mutations : {
         [CORE.SET_STATE](state, payload) {
             state.currentState = payload.state;
+        },
+        [CORE.SET_RENDERING_RESOLUTION_X](state, payload) {
+            state.options.rendering.resolution.x = payload.x
+        },
+        [CORE.SET_RENDERING_RESOLUTION_Y](state, payload) {
+            state.options.rendering.resolution.y = payload.y
+        },
+        [CORE.SET_RENDERING_FRAME_RANGE_START](state, payload) {
+            state.options.rendering.frameRange.start = payload.start
+        },
+        [CORE.SET_RENDERING_FRAME_RANGE_END](state, payload) {
+            state.options.rendering.frameRange.end = payload.end
         },
         [CORE.TOGGLE_VIEWPORT_OPTIONS](state, payload) {
             if(state.currentState == CST.STATES.VIEWPORT.HOVER)
