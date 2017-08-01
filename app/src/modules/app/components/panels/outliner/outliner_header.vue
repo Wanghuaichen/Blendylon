0c72f2df21f0d8420b637edc0c6e477f2150983f
Mel Florance <mel.florance@gmail.com>
2017-08-01 12:39:47 +0200

    Migrating old outliner component to match the new panels specs

---
+++ app/src/modules/app/components/panels/outliner/outliner_header.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="ui tiny menu inverted" id="outliner_search">
+        <div class="right menu">
+            <div class="ui right aligned category inverted search item">
+                <div class="ui transparent inverted icon input">
+                    <input class="prompt" type="text" placeholder="Filter scene...">
+                    <i class="search link icon"></i>
+                </div>
+                <div class="results"></div>
+            </div>
+        </div>
+    </div>
+</template>
+<script type="text/babel">
+    export default{
+        data(){
+            return {}
+        }
+    }
+</script>
