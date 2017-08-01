1f291409a6abf38859eadd087b3fb99c46e6d2fd
Mel Florance <mel.florance@gmail.com>
2017-08-01 12:34:11 +0200

    Fix indentation

--- app/src/core/directives/window.js
+++ app/src/core/directives/window.js
@@ -8,46 +8,41 @@
 import Vue from 'vue'
 
 Vue.directive('window-draggable',
-{
-    bind: el =>
     {
-        let excludes = ['INPUT', 'BUTTON', 'SELECT']
-
-        $(el).on('mousedown', e =>
-        {
-            if(excludes.indexOf(e.target.tagName) == -1)
-            {
-                let
-                dx = e.pageX - parseFloat(el.style.left),
-                dy = e.pageY - parseFloat(el.style.top);
-
-                el.style.cursor = 'move';
-
-                $(window).on('mousemove', e =>
-                {
-                    let x      = parseFloat(e.pageX) - dx;
-                    let y      = parseFloat(e.pageY) - dy;
-                    let width  = $(el).width();
-                    let height = $(el).height();
-
-                    if(x < 0) x = 0;
-                    if(y < 0) y = 0;
-
-                    if((x + width) > window.innerWidth)
-                        x = window.innerWidth - width;
-                    if((y + height) > window.innerHeight)
-                        y = window.innerHeight - height;
-
-                    el.style.left = x + 'px';
-                    el.style.top  = y + 'px';
-                })
-            }
-        });
-
-        $(window).on('mouseup', e =>
-        {
-            $(window).off('mousemove')
-            el.style.cursor = 'default'
-        })
-    }
-});
+        bind : el => {
+            let excludes = ['INPUT', 'BUTTON', 'SELECT']
+            
+            $(el).on('mousedown', e => {
+                if(excludes.indexOf(e.target.tagName) == -1) {
+                    let
+                        dx = e.pageX - parseFloat(el.style.left),
+                        dy = e.pageY - parseFloat(el.style.top);
+                    
+                    el.style.cursor = 'move';
+                    
+                    $(window).on('mousemove', e => {
+                        let x      = parseFloat(e.pageX) - dx;
+                        let y      = parseFloat(e.pageY) - dy;
+                        let width  = $(el).width();
+                        let height = $(el).height();
+                        
+                        if(x < 0) x = 0;
+                        if(y < 0) y = 0;
+                        
+                        if((x + width) > window.innerWidth)
+                            x = window.innerWidth - width;
+                        if((y + height) > window.innerHeight)
+                            y = window.innerHeight - height;
+                        
+                        el.style.left = x + 'px';
+                        el.style.top  = y + 'px';
+                    })
+                }
+            });
+            
+            $(window).on('mouseup', e => {
+                $(window).off('mousemove')
+                el.style.cursor = 'default'
+            })
+        }
+    });
