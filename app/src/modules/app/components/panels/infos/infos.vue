7affa9ad50eef9b361cd2794183227668c3c34b0
Mel Florance <mel.florance@gmail.com>
2017-08-01 12:39:25 +0200

    Creating new infos bar component

---
+++ app/src/modules/app/components/panels/infos/infos.vue
@@ -0,0 +1,62 @@
+<template>
+
+        <!--<small class="topbar_stats">-->
+        <!--<span class="text-muted">v{{version}}</span>-->
+        <!--<span class="text-muted">{{stats.vertices-2>1 ? 'Vertices' : 'Vertex'}}: {{stats.vertices - 2}}</span>-->
+        <!--<span class="text-muted">Mesh{{stats.meshes>1 ? 'es' : ''}}: {{stats.meshes}}</span>-->
+        <!--<span class="text-muted">Light{{stats.lights>1 ? 's' : ''}}: {{stats.lights}}</span>-->
+        <!--</small>-->
+    </div>
+</template>
+<script type="text/babel">
+var version = require('json!../../../../../../../package.json').version;
+export default{
+    mounted() {
+        this.version = version;
+//        this.stats = this.$children[0].stats;
+    },
+    data(){
+        return {
+            stats: {},
+            version: '',
+            menu: [
+                {
+                    name: "File",
+                    children: [
+                        {name: "New", icon: "", description: "CTRL N"},
+                        {name: "Open...", icon: "", description: "CTRL O"},
+                        {
+                            name: "Open recent...",
+                            icon: ""
+                        },
+                        {separator: true},
+                        {name: "Save", icon: ""},
+                        {name: "Save as...", icon: ""},
+                        {separator: true},
+                        {name: "Preferences...", icon: ""},
+                        {separator: true},
+                        {
+                            name: "Import",
+                            icon: ""
+                        },
+                        {
+                            name: "Export",
+                            icon: ""
+                        },
+                        {separator: true},
+                        {name: "Quit", icon: ""}
+                    ]
+                },
+                {
+                    name: "Game",
+                    children: []
+                },
+                {
+                    name: "Help",
+                    children: []
+                }
+            ]
+        }
+    }
+}
+</script>
---
+++ app/src/modules/app/components/panels/infos/infos_header.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="full-height full-width">
+        <div class="ui top pointing dropdown item" v-for="item in menu" style="float:left">
+            {{item.name}} <i class="dropdown icon"></i>
+            <div class="menu" v-if="item.children.length > 0" style="width:15rem;">
+                <a class="item" v-for="child in item.children" v-if="!child.separator" @click.prevent="">
+                    {{child.name}}
+                    <span class="description" v-if="child.description">
+                        {{child.description}}
+                    </span>
+                </a>
+                <div class="ui inverted divider" v-else></div>
+            </div>
+        </div>
+    </div>
+</template>
+<script type="text/babel">
+var version = require('json!../../../../../../../package.json').version;
+export default{
+    mounted() {
+        this.version = version;
+//        this.stats = this.$children[0].stats;
+    },
+    data(){
+        return {
+            stats: {},
+            version: '',
+            menu: [
+                {
+                    name: "File",
+                    children: [
+                        {name: "New", icon: "", description: "CTRL N"},
+                        {name: "Open...", icon: "", description: "CTRL O"},
+                        {
+                            name: "Open recent...",
+                            icon: ""
+                        },
+                        {separator: true},
+                        {name: "Save", icon: ""},
+                        {name: "Save as...", icon: ""},
+                        {separator: true},
+                        {name: "Preferences...", icon: ""},
+                        {separator: true},
+                        {
+                            name: "Import",
+                            icon: ""
+                        },
+                        {
+                            name: "Export",
+                            icon: ""
+                        },
+                        {separator: true},
+                        {name: "Quit", icon: ""}
+                    ]
+                },
+                {
+                    name: "Game",
+                    children: []
+                },
+                {
+                    name: "Help",
+                    children: []
+                }
+            ]
+        }
+    }
+}
+</script>
