e743934430e5986074e0d59abcafe00657221a9b
Mel Florance <mel.florance@gmail.com>
2017-08-01 12:34:26 +0200

    Fix indentation

--- app/src/core/events/Event.js
+++ app/src/core/events/Event.js
@@ -5,15 +5,13 @@
  * Written by Mel Florance <mel.florance@gmail.com>, 2016
  */
 
-export default class Event
-{
-	on(eventType, cb) {
-		this['_on' + eventType] = cb;
-	}
-
-	emit(eventType, data)
-	{
-		if(this['_on'+eventType])
-			this['_on' + eventType](data);
-	}
+export default class Event {
+    on(eventType, cb) {
+        this['_on' + eventType] = cb;
+    }
+    
+    emit(eventType, data) {
+        if(this['_on' + eventType])
+            this['_on' + eventType](data);
+    }
 }
