1f3a10817c6cea41bc6c3ea190ac93c21abd31dc
Mel Florance <mel.florance@gmail.com>
2017-08-01 12:51:58 +0200

    Upodate new scene store module

--- app/src/store/modules/scenes/scenes.js
+++ app/src/store/modules/scenes/scenes.js
@@ -16,157 +16,154 @@ import
     CAMERAS,
     GRIDS
 }
-from '../../mutations-types'
+    from '../../mutations-types'
 
 const Store =
-{
-    state: {
-        scenes   : [],
-        names    : 0,
-        current  : 0,
-        defaults : {
-            clearColor: new BABYLON.Color3(.3, .3, .3),
-            ambientColor: new BABYLON.Color3(.3, .3, .3)
-        }
-    },
-    getters: {
-        getScenes          : state => state.scenes,
-        getCurrentSceneId  : state => {
-            if(typeof state.scenes[state.current] != 'undefined')
-                return state.scenes[state.current].id
-        },
-        getCurrentScene    : state => state.scenes[state.current],
-        getSceneStatistics : state => {
-            let scene = state.scenes[state.current];
-            if(typeof scene != 'undefined') {
-                let stats = {};
-                let internal = {
-                    vertices : 0,
-                    meshes   : 0,
-                    lights   : 0,
-                    cameras  : 0
-                }
-
-                scene.meshes.forEach(mesh => {
-                    if(mesh.type == CST.OBJECTS.VIEWPORT_MESH) {
-                        internal.vertices += mesh.getTotalVertices()
-                        internal.meshes++;
-                    }
-                })
-
-                scene.lights.forEach(light => light.type == CST.OBJECTS.VIEWPORT_LIGHT ? internal.lights++ : null);
-                scene.cameras.forEach(camera => camera.type == CST.OBJECTS.VIEWPORT_CAMERA ? internal.cameras++ : null);
-
-                stats.vertices  = scene.getTotalVertices() - internal.vertices;
-                stats.meshes    = scene.meshes.length - internal.meshes;
-                stats.lights    = scene.lights.length - internal.lights;
-                stats.cameras   = scene.cameras.length - internal.cameras;
-                stats.materials = scene.materials.length - 2 < 0 ? 0 : scene.materials.length - 2;
-                stats.textures  = scene.textures.length - 2;
-
-                return stats;
-            }
-            else return {}
-        },
-    },
-    actions: {
-        setActiveCamera(store, camera) {
-            store.commit(SCENES.SET_ACTIVE_CAMERA, camera);
-        },
-        switchScene(store, id) {
-            store.commit(SCENES.SWITCH, id);
-            store.commit(CAMERAS.SWITCH, id);
-            store.commit(CURSORS.SWITCH, id);
-        },
-        addScene(store, payload) {
-            store.commit(SCENES.ADD, payload);
-            let currentScene = store.state.scenes[store.state.current];
-            //Todo: Add scene components
-            // + Cursor
-            // + Camera
-            // - Grid
-            // - Selection
-
-            let settings = {
-                cursor: {
-                    scene: currentScene,
-                    radius: 10
-                },
-                camera: {
-                    scene: currentScene,
-                    canvas: payload.canvas
-                },
-                grid: {
-                    scene: currentScene
-                }
-            };
-
-            for(var setting in settings)
-                settings[setting].init = true;
-
-            store.commit(CAMERAS.ADD_VIEWPORT, settings.camera);
-            store.commit(GRIDS.ADD,            settings.grid);
-            store.commit(CURSORS.CREATE,       settings.cursor);
-            store.commit(CURSORS.RESIZE, {
-                sceneId: settings.cursor.scene.id,
-                radius: settings.cursor.radius
-            });
-        },
-        deleteScene(store, payload) {
-            store.commit(GRIDS.DELETE,   payload);
-            store.commit(CURSORS.DELETE, payload);
-            store.commit(SCENES.DELETE,  payload);
-        },
-        updateObjectsShadows(store, payload) {
-            store.commit(SCENES.UPDATE_OBJECTS_SHADOWS, payload);
-        }
-    },
-    mutations: {
-        [SCENES.UPDATE_OBJECTS_SHADOWS](state, payload) {
-            state.scenes[state.current].meshes.forEach(mesh =>
-            {
-                if(mesh.receiveShadows === true)
-                {
-                    let renderList = payload.generator.getShadowMap().renderList;
-                    let index = renderList.findIndex(object => object.name == mesh.name);
-
-                    if(index == -1 && mesh.receiveShadows == true)
-                        renderList.push(mesh)
-                }
-            });
-        },
-        [SCENES.SET_ACTIVE_CAMERA](state, camera) {
-            state.scenes[state.current].activeCamera = camera;
-        },
-        [SCENES.SWITCH](state, id) {
-            state.current = getSceneIndex(id)
-        },
-        [SCENES.ADD](state, payload)
-        {
-            let scene          = new BABYLON.Scene(payload.engine);
-            scene.id           = shortid.generate();
-            scene.name         = setSceneName('Scene');
-            scene.clearColor   = state.defaults.clearColor;
-            scene.ambientColor = state.defaults.ambientColor;
-
-            scene.renderLoop = function() { this.render() }
-
-            state.scenes.push(scene);
-            state.current = getLastIndex()
-        },
-        [SCENES.DELETE](state, payload) {
-            let index = state.scenes.findIndex(scene => scene.id == payload.sceneId);
-            if(index >= 0)
-            {
-                let scene = state.scenes[index];
-                if(typeof scene.dispose == 'function')
-                    scene.dispose();
-                state.scenes.splice(index, 1);
-                state.current = getLastIndex()
-            }
-        }
-    }
-}
+      {
+          state     : {
+              scenes   : [],
+              names    : 0,
+              current  : 0,
+              defaults : {
+                  clearColor   : new BABYLON.Color3(.25, .25, .25),
+                  ambientColor : new BABYLON.Color3(.4, .4, .4)
+              }
+          },
+          getters   : {
+              getScenes          : state => state.scenes,
+              getCurrentSceneId  : state => {
+                  if(typeof state.scenes[state.current] != 'undefined')
+                      return state.scenes[state.current].id
+              },
+              getCurrentScene    : state => state.scenes[state.current],
+              getSceneStatistics : state => {
+                  let scene = state.scenes[state.current];
+                  if(typeof scene != 'undefined') {
+                      let stats    = {};
+                      let internal = {
+                          vertices : 0,
+                          meshes   : 0,
+                          lights   : 0,
+                          cameras  : 0
+                      }
+                
+                      scene.meshes.forEach(mesh => {
+                          if(mesh.type == CST.OBJECTS.VIEWPORT_MESH) {
+                              internal.vertices += mesh.getTotalVertices()
+                              internal.meshes++;
+                          }
+                      })
+                
+                      scene.lights.forEach(light => light.type == CST.OBJECTS.VIEWPORT_LIGHT ? internal.lights++ : null);
+                      scene.cameras.forEach(camera => camera.type == CST.OBJECTS.VIEWPORT_CAMERA ? internal.cameras++ : null);
+                
+                      stats.vertices  = scene.getTotalVertices() - internal.vertices;
+                      stats.meshes    = scene.meshes.length - internal.meshes;
+                      stats.lights    = scene.lights.length - internal.lights;
+                      stats.cameras   = scene.cameras.length - internal.cameras;
+                      stats.materials = scene.materials.length - 2 < 0 ? 0 : scene.materials.length - 2;
+                      stats.textures  = scene.textures.length - 2;
+                
+                      return stats;
+                  }
+                  else return {}
+              },
+          },
+          actions   : {
+              setActiveCamera(store, camera) {
+                  store.commit(SCENES.SET_ACTIVE_CAMERA, camera);
+              },
+              switchScene(store, id) {
+                  store.commit(SCENES.SWITCH, id);
+                  store.commit(CAMERAS.SWITCH, id);
+                  store.commit(CURSORS.SWITCH, id);
+              },
+              addScene(store, payload) {
+                  store.commit(SCENES.ADD, payload);
+                  let currentScene = store.state.scenes[store.state.current];
+                  //Todo: Add scene components
+                  // + Cursor
+                  // + Camera
+                  // - Grid
+                  // - Selection
+            
+                  let settings = {
+                      cursor : {
+                          scene  : currentScene,
+                          radius : 10
+                      },
+                      camera : {
+                          scene  : currentScene,
+                          canvas : payload.canvas
+                      },
+                      grid   : {
+                          scene : currentScene
+                      }
+                  };
+            
+                  for(var setting in settings)
+                      settings[setting].init = true;
+            
+                  store.commit(CAMERAS.ADD_VIEWPORT, settings.camera);
+                  store.commit(GRIDS.ADD, settings.grid);
+                  store.commit(CURSORS.CREATE, settings.cursor);
+                  store.commit(CURSORS.RESIZE, {
+                      sceneId : settings.cursor.scene.id,
+                      radius  : settings.cursor.radius
+                  });
+              },
+              deleteScene(store, payload) {
+                  store.commit(GRIDS.DELETE, payload);
+                  store.commit(CURSORS.DELETE, payload);
+                  store.commit(SCENES.DELETE, payload);
+              },
+              updateObjectsShadows(store, payload) {
+                  store.commit(SCENES.UPDATE_OBJECTS_SHADOWS, payload);
+              }
+          },
+          mutations : {
+              [SCENES.UPDATE_OBJECTS_SHADOWS](state, payload) {
+                  state.scenes[state.current].meshes.forEach(mesh => {
+                      if(mesh.receiveShadows === true) {
+                          let renderList = payload.generator.getShadowMap().renderList;
+                          let index      = renderList.findIndex(object => object.name == mesh.name);
+                    
+                          if(index == -1 && mesh.receiveShadows == true)
+                              renderList.push(mesh)
+                      }
+                  });
+              },
+              [SCENES.SET_ACTIVE_CAMERA](state, camera) {
+                  state.scenes[state.current].activeCamera = camera;
+              },
+              [SCENES.SWITCH](state, id) {
+                  state.current = getSceneIndex(id)
+              },
+              [SCENES.ADD](state, payload)
+              {
+                  let scene          = new BABYLON.Scene(payload.engine);
+                  scene.id           = shortid.generate();
+                  scene.name         = setSceneName('Scene');
+                  scene.clearColor   = state.defaults.clearColor;
+                  scene.ambientColor = state.defaults.ambientColor;
+            
+                  scene.renderLoop = function() { this.render() }
+            
+                  state.scenes.push(scene);
+                  state.current = getLastIndex()
+              },
+              [SCENES.DELETE](state, payload) {
+                  let index = state.scenes.findIndex(scene => scene.id == payload.sceneId);
+                  if(index >= 0) {
+                      let scene = state.scenes[index];
+                      if(typeof scene.dispose == 'function')
+                          scene.dispose();
+                      state.scenes.splice(index, 1);
+                      state.current = getLastIndex()
+                  }
+              }
+          }
+      }
 
 const getLastIndex = () => {
     let length = Store.state.scenes.length - 1;
@@ -179,13 +176,13 @@ const getSceneIndex = (id) => {
 };
 
 const setSceneName = (name) => {
-    name = Utility.capitalize(name);
+    name       = Utility.capitalize(name);
     let exists = Store.state.scenes.find(scene => scene.name == name);
-
+    
     if(!exists) return name;
     else {
         Store.state.names++;
-
+        
         return name + '.' + Utility.pad(Store.state.names, 3)
     }
 };
