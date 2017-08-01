24bd64ac307f071a968637187335d98f591e410d
Mel Florance <mel.florance@gmail.com>
2017-08-01 12:40:18 +0200

    Updating rendering panel

--- app/src/modules/app/components/panels/properties/rendering/rendering.vue
+++ app/src/modules/app/components/panels/properties/rendering/rendering.vue
@@ -1,56 +1,171 @@
 <template>
     <div style="padding: 0" class="scrollable-y full-height">
-        <div class="ui two wide column" style="padding: 15px;">
+        <div class="ui one wide column">
             <div class="ui inverted accordion">
                 <div class="title active accordion-header noselect">
-                    <i class="dropdown icon"></i> Mobile Device Orientation
+                    <i class="dropdown icon"></i> Render
                 </div>
                 <div class="content active">
-                    <div class="ui inverted mini fluid labeled input input-transform slider checkbox right input-slider" @click="toggleServer()">
-                        <div class="ui black label fixed noselect" style="width: 50%">
-                            Server status
+                    <div class="ui grid">
+                        <div class="column">
+                            <div class="two ui buttons">
+                                <button class="ui labeled fluid inverted icon button">
+                                    <i class="icon"><img src="assets/img/icons/rendering.png" style="margin-top:5px"></i>
+                                    Render
+                                </button>
+                                <button class="ui labeled fluid inverted icon button" >
+                                    <i class="icon"><img src="assets/img/icons/rendering.png" style="margin-top:5px"></i>
+                                    Animation
+                                </button>
+                            </div>
                         </div>
-                        <div class="ui label" style="border-radius:0;"
-                             :class="{
-                                'red': server.started == false,
-                                'olive': server.started == true
-                            }">
-                            <i class="icon" :class="{
-                                'remove': server.started == false,
-                                'checkmark': server.started == true
-                            }"></i>
-                            <span v-if="server.started">STARTED</span>
-                            <span v-if="!server.started">STOPPED</span>
-                        </div>
-                        <input
-                            type="checkbox"
-                            name="server_status"
-                            :value="server.started">
-                    </div>
-                    <InputText
-                        :attr="server.address"
-                        label="Server address"
-                        @submitted="setServerAddress">
-                    </InputText>
-                    <div class="ui inverted mini right fluid labeled input input-transform">
-                        <div class="ui black label fixed noselect" style="width: 50%">
-                            Server port
-                        </div>
-                        <input
-                            type="number"
-                            name="server_port"
-                            class="input-draggable"
-                            min="1000"
-                            step="1"
-                            v-input-draggable
-                            :value="server.port"
-                            @mousemove="setServerPort"
-                            @input="setServerPort"
-                            title="Set server port">
                     </div>
                 </div>
             </div>
+            <div class="ui inverted accordion">
+                <div class="title active accordion-header noselect">
+                    <i class="dropdown icon"></i> Dimensions
+                </div>
+                <div class="content active">
+                    <div class="ui two column grid">
+                        <div class="column">
+                            <div class="text-center accordion-title noselect">Resolution</div>
+                            <div class="ui inverted mini right fluid labeled input input-transform">
+                                <div class="ui red label noselect">X</div>
+                                <input
+                                    type="number"
+                                    name="rendering_resolution_x"
+                                    class="input-draggable"
+                                    v-input-draggable
+                                    step="1"
+                                    min="0"
+                                    :value="coreOptions.rendering.resolution.x"
+                                    @inputDrag.prevent="updateRenderingResolutionX"
+                                    @keyup.enter.prevent="updateRenderingResolutionX"
+                                    title="Update rendering width">
+                            </div>
+                            <div class="ui inverted mini right fluid labeled input input-transform">
+                                <div class="ui green label noselect">Y</div>
+                                <input
+                                    type="number"
+                                    name="rendering_resolution_y"
+                                    class="input-draggable"
+                                    v-input-draggable
+                                    step="1"
+                                    min="0"
+                                    :value="coreOptions.rendering.resolution.y"
+                                    @inputDrag.prevent="updateRenderingResolutionY"
+                                    @keyup.enter.prevent="updateRenderingResolutionY"
+                                    title="Update rendering height">
+                            </div>
+                        </div>
+
+                        <div class="column">
+                            <div class="text-center accordion-title noselect">Frame Range</div>
+                            <div class="ui inverted mini right fluid labeled input input-transform">
+                                <div class="ui grey label noselect" style="width: 33.3%">Start</div>
+                                <input
+                                        type="number"
+                                        name="rendering_frame_range_start"
+                                        class="input-draggable"
+                                        v-input-draggable
+                                        step="1"
+                                        min="0"
+                                        :value="coreOptions.rendering.frameRange.start"
+                                        @inputDrag.prevent="updateRenderingFrameRangeStart"
+                                        @keyup.enter.prevent="updateRenderingFrameRangeStart"
+                                        title="Update rendering start frame">
+                            </div>
+                            <div class="ui inverted mini right fluid labeled input input-transform">
+                                <div class="ui grey label noselect" style="width: 33.3%">End</div>
+                                <input
+                                        type="number"
+                                        name="rendering_frame_range_end"
+                                        class="input-draggable"
+                                        v-input-draggable
+                                        step="1"
+                                        min="0"
+                                        :value="coreOptions.rendering.frameRange.end"
+                                        @inputDrag.prevent="updateRenderingFrameRangeEnd"
+                                        @keyup.enter.prevent="updateRenderingFrameRangeEnd"
+                                        title="Update rendering end frame">
+                            </div>
+                        </div>
+                    </div>
+                    <br>
+                </div>
+
+            </div>
+            <div class="ui inverted accordion">
+                <div class="title active accordion-header noselect">
+                    <i class="dropdown icon"></i> Output
+                </div>
+                <div class="content active">
+                    <div class="ui column grid">
+                        <div class="column">
+                            <div class="ui inverted mini fluid input input-transform right">
+                                <input type="text" class="folder_selector"  readonly>
+                                <input type="file" webkitdirectory directory multiple style="display:none">
+                                <div class="ui icon button">
+                                    <i class="attach icon"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!--<div class="ui inverted accordion">-->
+                <!--<div class="title active accordion-header noselect">-->
+                    <!--<i class="dropdown icon"></i> Mobile Device Orientation-->
+                <!--</div>-->
+                <!--<div class="content active">-->
+                    <!--<div class="ui inverted mini fluid labeled input input-transform slider checkbox right input-slider" @click="toggleServer()">-->
+                        <!--<div class="ui black label fixed noselect" style="width: 50%">-->
+                            <!--Server status-->
+                        <!--</div>-->
+                        <!--<div class="ui label" style="border-radius:0;"-->
+                             <!--:class="{-->
+                                <!--'red': server.started == false,-->
+                                <!--'olive': server.started == true-->
+                            <!--}">-->
+                            <!--<i class="icon" :class="{-->
+                                <!--'remove': server.started == false,-->
+                                <!--'checkmark': server.started == true-->
+                            <!--}"></i>-->
+                            <!--<span v-if="server.started">STARTED</span>-->
+                            <!--<span v-if="!server.started">STOPPED</span>-->
+                        <!--</div>-->
+                        <!--<input-->
+                            <!--type="checkbox"-->
+                            <!--name="server_status"-->
+                            <!--:value="server.started">-->
+                    <!--</div>-->
+                    <!--<InputText-->
+                        <!--:attr="server.address"-->
+                        <!--label="Server address"-->
+                        <!--@submitted="setServerAddress">-->
+                    <!--</InputText>-->
+                    <!--<div class="ui inverted mini right fluid labeled input input-transform">-->
+                        <!--<div class="ui black label fixed noselect" style="width: 50%">-->
+                            <!--Server port-->
+                        <!--</div>-->
+                        <!--<input-->
+                            <!--type="number"-->
+                            <!--name="server_port"-->
+                            <!--class="input-draggable"-->
+                            <!--min="1000"-->
+                            <!--step="1"-->
+                            <!--v-input-draggable-->
+                            <!--:value="server.port"-->
+                            <!--@mousemove="setServerPort"-->
+                            <!--@input="setServerPort"-->
+                            <!--title="Set server port">-->
+                    <!--</div>-->
+                <!--</div>-->
+            <!--</div>-->
         </div>
+
     </div>
 </template>
 <script>
@@ -71,7 +186,8 @@ export default {
         }
     },
     computed: mapGetters({
-        currentCamera: 'getCurrentCamera'
+        currentCamera: 'getCurrentCamera',
+        coreOptions: 'getCoreOptions'
     }),
     created() {
 //        require('dns').lookup(require('os').hostname(), (err, addr, fam) => {
@@ -79,6 +195,32 @@ export default {
 //        })
     },
     methods: {
+        ...mapActions({
+            setRenderingResolutionX     : 'setRenderingResolutionX',
+            setRenderingResolutionY     : 'setRenderingResolutionY',
+            setRenderingFrameRangeStart : 'setRenderingFrameRangeStart',
+            setRenderingFrameRangeEnd   : 'setRenderingFrameRangeEnd'
+        }),
+        updateRenderingResolutionX() {
+            this.setRenderingResolutionX({
+                x: parseFloat(window.$('input[name="rendering_resolution_x"]').val())
+            })
+        },
+        updateRenderingResolutionY() {
+            this.setRenderingResolutionY({
+                y: parseFloat(window.$('input[name="rendering_resolution_y"]').val())
+            })
+        },
+        updateRenderingFrameRangeStart() {
+            this.setRenderingFrameRangeStart({
+                start: parseFloat(window.$('input[name="rendering_frame_range_start"]').val())
+            })
+        },
+        updateRenderingFrameRangeEnd() {
+            this.setRenderingFrameRangeEnd({
+                end: parseFloat(window.$('input[name="rendering_frame_range_end"]').val())
+            })
+        },
         toggleServer()
         {
             if(!this.server.started)
